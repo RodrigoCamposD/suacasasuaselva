0edc17d659b906d74eb432fe74c0ce5d7fa179cc
RodrigoCamposD <campos6@gmail.com>
2023-04-07 22:11:36 -0300

    foulder four

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ const footerText = document.getElementById("footer-text");
 
 const year = new Date();
 footerText.innerHTML = `Sua casa Sua Selva &reg; ${year.getFullYear()}. Todos os Direitos Reservados`;
+
+const toTop = document.getElementById("to-top");
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 120 ||
+    document.documentElement.scrollTop > 120
+  ) {
+    toTop.classList.remove("d-none");
+  } else {
+    toTop.classList.add("d-none");
+  }
+};
