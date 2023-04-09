a5e35ccc67d842a12df8172672d8dc1383c24121
RodrigoCamposD <campos6@gmail.com>
2023-04-09 16:33:30 -0300

    content update

--- src/js/main.js
+++ src/js/main.js
@@ -35,21 +35,37 @@ buttons.forEach((button) => {
 
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add(
-        "animate__animated",
-        "animate__rubberBand",
-        "animate__slow"
-      );
-    } else {
-      entry.target.classList.remove(
-        "animate__animated",
-        "animate__rubberBand",
-        "animate__slow"
-      );
+    if (entry.target.classList.contains("rubberBand")) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add(
+          "animate__animated",
+          "animate__rubberBand",
+          "animate__slow"
+        );
+      } else {
+        entry.target.classList.remove(
+          "animate__animated",
+          "animate__rubberBand",
+          "animate__slow"
+        );
+      }
+    } else if (entry.target.classList.contains("fadeInLeft")) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate__animated", "animate__fadeInLeft");
+        observer.unobserve(entry.target);
+      }
+    } else if (entry.target.classList.contains("fadeInUp")) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate__animated", "animate__fadeInUp");
+        observer.unobserve(entry.target);
+      }
     }
   });
 });
 
-const targetElement = document.querySelector("#target");
-observer.observe(targetElement);
+const rubberBand = document.querySelectorAll(".rubberBand");
+const fadeInLeft = document.querySelectorAll(".fadeInLeft");
+const fadeInUp = document.querySelectorAll(".fadeInUp");
+rubberBand.forEach((el) => observer.observe(el));
+fadeInLeft.forEach((el) => observer.observe(el));
+fadeInUp.forEach((el) => observer.observe(el));
